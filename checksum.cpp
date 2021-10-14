7e5ce280079a7370379db97232eda2425ee4c906
xaviermiles <xaviermiles377@gmail.com>
2021-10-14 17:05:00 +1300

    Change checksum functions into classes

--- checksum.cpp
+++ checksum.cpp
@@ -4,36 +4,42 @@
 
 void test_luhn_algorithm()
 {
+    std::cout << "Testing Luhn" << std::endl;
     long int test_nums[2] = {
         79927398713, // should pass
         123456789    // should not pass
     };
     for (auto &x : test_nums) {
-        bool passes_test = luhn_algorithm(x);
+        Luhn luhn;
+        bool passes_test = luhn.validate_check_dgt(x);
         std::cout << x << " - " << (passes_test ? "Yay" : "Nay") << std::endl;
     }
 }
 
 void test_verhoeff_algorithm()
 {
+    std::cout << "Testing Verhoeff" << std::endl;
     long int test_nums[2] = {
         2363,     // should pass
         123456789 // shouldn't pass
     };
     for (auto &x : test_nums) {
-        bool passes_test = verhoeff_algorithm(x);
+        Verhoeff verhoeff;
+        bool passes_test = verhoeff.validate_check_dgt(x);
         std::cout << x << " - " << (passes_test ? "Yay" : "Nay") << std::endl;
     }
 }
 
 void test_damm_algorithm()
 {
+    std::cout << "Testing Damm" << std::endl;
     long int test_nums[2] = {
         5724,     // should pass
         123456789 // shouldn't pass
     };
     for (auto &x : test_nums) {
-        bool passes_test = damm_algorithm(x);
+        Damm damm;
+        bool passes_test = damm.validate_check_dgt(x);
         std::cout << x << " - " << (passes_test ? "Yay" : "Nay") << std::endl;
     }
 }
--- checksum.h
+++ checksum.h
@@ -3,131 +3,137 @@
 #include <stdio.h>
 #include <iostream>
 
-bool luhn_algorithm(long int num)
-{
+class AbstractChecksum {
+public:
+    // int calculate_check_dgt(long int num);
+    bool validate_check_dgt(long int num);
+};
+
+class Luhn: public AbstractChecksum {
     /*
      * https://en.wikipedia.org/wiki/Luhn_algorithm
-     * Used to check whether card number was mistyped (along with other uses).
-     * Relatively simple check - "it will detect any single-digit error, as well
-     * as well as almost all transpositions of adjacent digits."
      */
-    auto num_copy = num / 10;
-    std::vector<int> check_num_dgts;
-    while (num_copy) {
-        check_num_dgts.push_back(num_copy % 10);
-        num_copy /= 10;
-    }
-    reverse(check_num_dgts.begin(), check_num_dgts.end());
 
-    int double_dgt;
-    for (std::vector<int>::iterator it = check_num_dgts.end() - 1;
-         it >= check_num_dgts.begin(); ) {
-        double_dgt = *it * 2;
-        *it = 0;
-        while (double_dgt) {
-            *it += (double_dgt % 10);
-            double_dgt /= 10;
+public:
+    bool validate_check_dgt(long int num) {
+        auto num_copy = num / 10;
+        std::vector<int> check_num_dgts;
+        while (num_copy) {
+            check_num_dgts.push_back(num_copy % 10);
+            num_copy /= 10;
         }
-        it = it - 2;
+        reverse(check_num_dgts.begin(), check_num_dgts.end());
+
+        int double_dgt;
+        for (std::vector<int>::iterator it = check_num_dgts.end() - 1;
+             it >= check_num_dgts.begin(); ) {
+            double_dgt = *it * 2;
+            *it = 0;
+            while (double_dgt) {
+                *it += (double_dgt % 10);
+                double_dgt /= 10;
+            }
+            it = it - 2;
+        }
+
+        int sum_transformed_dgts = 0;
+        for (int &x : check_num_dgts)
+            sum_transformed_dgts += x;
+
+        int luhn_num = 10 - sum_transformed_dgts % 10;
+        int check_digit = num % 10;  // final digit in original number
+        bool check_digit_correct = check_digit == luhn_num;
+
+        return check_digit_correct;
     }
+};
 
-    int sum_transformed_dgts = 0;
-    for (int &x : check_num_dgts)
-        sum_transformed_dgts += x;
-
-    int luhn_num = 10 - sum_transformed_dgts % 10;
-    int check_digit = num % 10;  // final digit in original number
-    bool check_digit_correct = check_digit == luhn_num;
-
-    return check_digit_correct;
-}
-
-bool verhoeff_algorithm(long int num)
-{
+class Verhoeff: public AbstractChecksum {
     /*
      * https://en.wikipedia.org/wiki/Verhoeff_algorithm
-     * Checksum formula for error detection
-     * "detects all single-digit errors and all transposition errors involving
-     * two adjacent digits"
      *
      * Can be performed using three tables:
      * d: Cayley table of the dihedral group D_5
      * inv: multiplicative inverse of the index
      * p: specific permutation applied iteratively
      */
-    int d[10][10] = {
-        {0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
-        {1, 2, 3, 4, 0, 6, 7, 8, 9, 5},
-        {2, 3, 4, 0, 1, 7, 8, 9, 5, 6},
-        {3, 4, 0, 1, 2, 8, 9, 5, 6, 7},
-        {4, 0, 1, 2, 3, 9, 5, 6, 7, 8},
-        {5, 9, 8, 7, 6, 0, 4, 3, 2, 1},
-        {6, 5, 9, 8, 7, 1, 0, 4, 3, 2},
-        {7, 6, 5, 9, 8, 2, 1, 0, 4, 3},
-        {8, 7, 6, 5, 9, 3, 2, 1, 0, 4},
-        {9, 8, 7, 6, 5, 4, 3, 2, 1, 0}
-    };
-    int inv[10] = {0, 4, 3, 2, 1, 5, 6, 7, 8, 9};
-    int p[8][10] = {
-        {0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
-        {1, 5, 7, 6, 2, 8, 3, 0, 9, 4},
-        {5, 8, 0, 3, 7, 9, 6, 1, 4, 2},
-        {8, 9, 1, 6, 0, 4, 3, 5, 2, 7},
-        {9, 4, 5, 3, 1, 2, 6, 8, 7, 0},
-        {4, 2, 8, 6, 5, 7, 3, 9, 0, 1},
-        {2, 7, 9, 3, 8, 0, 6, 4, 1, 5},
-        {7, 0, 4, 6, 9, 1, 3, 2, 5, 8}
-    };
+     int d[10][10] = {
+         {0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+         {1, 2, 3, 4, 0, 6, 7, 8, 9, 5},
+         {2, 3, 4, 0, 1, 7, 8, 9, 5, 6},
+         {3, 4, 0, 1, 2, 8, 9, 5, 6, 7},
+         {4, 0, 1, 2, 3, 9, 5, 6, 7, 8},
+         {5, 9, 8, 7, 6, 0, 4, 3, 2, 1},
+         {6, 5, 9, 8, 7, 1, 0, 4, 3, 2},
+         {7, 6, 5, 9, 8, 2, 1, 0, 4, 3},
+         {8, 7, 6, 5, 9, 3, 2, 1, 0, 4},
+         {9, 8, 7, 6, 5, 4, 3, 2, 1, 0}
+     };
+     int inv[10] = {0, 4, 3, 2, 1, 5, 6, 7, 8, 9};
+     int p[8][10] = {
+         {0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+         {1, 5, 7, 6, 2, 8, 3, 0, 9, 4},
+         {5, 8, 0, 3, 7, 9, 6, 1, 4, 2},
+         {8, 9, 1, 6, 0, 4, 3, 5, 2, 7},
+         {9, 4, 5, 3, 1, 2, 6, 8, 7, 0},
+         {4, 2, 8, 6, 5, 7, 3, 9, 0, 1},
+         {2, 7, 9, 3, 8, 0, 6, 4, 1, 5},
+         {7, 0, 4, 6, 9, 1, 3, 2, 5, 8}
+     };
 
-    // Create array out of the individual digits of *num*, right to left
-    std::vector<int> num_dgts;
-    while (num) {
-        num_dgts.push_back(num % 10);
-        num /= 10;
+public:
+    bool validate_check_dgt(long int num) {
+        // Create array out of the individual digits of *num*, right to left
+        std::vector<int> num_dgts;
+        while (num) {
+            num_dgts.push_back(num % 10);
+            num /= 10;
+        }
+        // For each index i=1,2,...,len(num_dgts): c = d(c, p(i mod 8, num[i]))
+        int c = 0;
+        for (auto idx = 0; idx != num_dgts.size(); idx++) {
+            c = d[c][p[num_dgts[idx] % 8][idx]];
+        }
+        // *num* is valid iff c=0
+        bool check_dgt_correct = c == 0;
+
+        return check_dgt_correct;
     }
-    // For each index i=1,2,...,len(num_dgts): c = d(c, p(i mod 8, num[i]))
-    int c = 0;
-    for (auto idx = 0; idx != num_dgts.size(); idx++) {
-        c = d[c][p[num_dgts[idx] % 8][idx]];
-    }
-    // *num* is valid iff c=0
-    bool check_dgt_correct = c == 0;
+};
 
-    return check_dgt_correct;
-}
-
-bool damm_algorithm(long int num)
-{
+class Damm: public AbstractChecksum {
     /*
      * https://en.wikipedia.org/wiki/Damm_algorithm
-     * Quasigroup table from pg. 98 of
-     * http://archiv.ub.uni-marburg.de/diss/z2004/0516/pdf/dhmd.pdf
+     * Other quasigroup tables are possible
      */
-    int quasigroup_tbl[10][10] = {
-        {0, 3, 1, 7, 5, 9, 8, 6, 4, 2},
-        {7, 0, 9, 2, 1, 5, 4, 8, 6, 3},
-        {4, 2, 0, 6, 8, 7, 1, 3, 5, 9},
-        {1, 7, 5, 0, 9, 8, 3, 4, 2, 6},
-        {6, 1, 2, 3, 0, 4, 5, 9, 7, 8},
-        {3, 6, 7, 4, 2, 0, 9, 5, 8, 1},
-        {5, 8, 6, 9, 7, 2, 0, 1, 3, 4},
-        {8, 9, 4, 5, 3, 6, 2, 0, 1, 7},
-        {9, 4, 3, 8, 6, 1, 7, 2, 0, 5},
-        {2, 5, 8, 1, 4, 3, 6, 7, 9, 0}
-    };
+     int quasigroup_tbl[10][10] = {
+         {0, 3, 1, 7, 5, 9, 8, 6, 4, 2},
+         {7, 0, 9, 2, 1, 5, 4, 8, 6, 3},
+         {4, 2, 0, 6, 8, 7, 1, 3, 5, 9},
+         {1, 7, 5, 0, 9, 8, 3, 4, 2, 6},
+         {6, 1, 2, 3, 0, 4, 5, 9, 7, 8},
+         {3, 6, 7, 4, 2, 0, 9, 5, 8, 1},
+         {5, 8, 6, 9, 7, 2, 0, 1, 3, 4},
+         {8, 9, 4, 5, 3, 6, 2, 0, 1, 7},
+         {9, 4, 3, 8, 6, 1, 7, 2, 0, 5},
+         {2, 5, 8, 1, 4, 3, 6, 7, 9, 0}
+     };
 
-    std::vector<int> num_dgts;
-    while (num) {
-        num_dgts.push_back(num % 10);
-        num /= 10;
+public:
+    bool validate_check_dgt(long int num) {
+        std::vector<int> num_dgts;
+        while (num) {
+            num_dgts.push_back(num % 10);
+            num /= 10;
+        }
+        reverse(num_dgts.begin(), num_dgts.end());
+
+        int n = 0;
+        for (auto it = num_dgts.begin(); it != num_dgts.end(); it++) {
+            n = quasigroup_tbl[n][*it];
+        }
+        bool check_dgt_correct = n == 0;
+
+        return check_dgt_correct;
     }
-    reverse(num_dgts.begin(), num_dgts.end());
-
-    int n = 0;
-    for (auto it = num_dgts.begin(); it != num_dgts.end(); it++) {
-        n = quasigroup_tbl[n][*it];
-    }
-    bool check_dgt_correct = n == 0;
-
-    return check_dgt_correct;
-}
+};
